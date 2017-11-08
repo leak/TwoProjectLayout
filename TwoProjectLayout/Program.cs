4cad7094212770f495ab7d4d5697ce407aff53a8
leak <leak@bitmx.net>
2017-11-08 09:23:13 +0100

    Add sample project

---
+++ TwoProjectLayout/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TwoProjectLayout
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            Console.WriteLine("CURRENT DIRECTORY: " + Directory.GetCurrentDirectory());
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+    }
+}
